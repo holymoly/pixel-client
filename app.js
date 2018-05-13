47e14aa30af438352e71adaf5c3f3ac392dd7284
Maurice Streubel <holymoly@holybook.local>
2018-05-13 19:51:56 +0200

    provide parameter to define amount of sockets

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var net = require('net');
 var Jimp = require("jimp");
 const fs = require('fs');
 
+var clients = new Array;
+
 var data = [];
 var picture = '';
 
@@ -9,46 +11,12 @@ var picture = '';
 process.argv.forEach(function (val, index, array) {
   console.log(index + ': ' + val);
 });
-
   
-function start(){
-  Jimp.read(process.argv[2]).then(function (image) {
-    image.scan(0, 0, image.bitmap.width, image.bitmap.height, function (x, y, idx) {
-      // x, y is the position of this pixel on the image
-      // idx is the position start position of this rgba tuple in the bitmap Buffer
-      // this is the image
-
-      var red   = this.bitmap.data[ idx + 0 ];
-      var green = this.bitmap.data[ idx + 1 ];
-      var blue  = this.bitmap.data[ idx + 2 ];
-      var alpha = this.bitmap.data[ idx + 3 ];
-
-      var xoffset = x + parseInt(process.argv[4], 10);
-      var yoffset = y + parseInt(process.argv[5], 10);
-
-      //console.log('Pixel x: ' + x + ' y: ' + y + ' R: ' + red + ' G: ' + green + ' B: ' + blue + ' Alpha: ' + alpha);
-      // rgba values run from 0 - 255
-      // e.g. this.bitmap.data[idx] = 0; // removes red from this pixel
-      //
-      //if(red != 255 && green != 255 && blue != 255){
-        data.push('PX ' + xoffset + ' ' + yoffset + ' ' + red.toString(16) + green.toString(16) + blue.toString(16)+ alpha.toString(16));
-      //}
-      
-      if(x == image.bitmap.width-1 &&
-          y == image.bitmap.height-1) {
-          picture = shuffle(data).join('\n') + '\n';
-          writeToFile(picture);
-
-          console.log('IS IPV6:' + net.isIPv6(process.argv[3]));
-          client.connect(1234,process.argv[3] , function() {
-            console.log('Connected');
-            
-            writePixel(picture);
-          });
-      }
-    });
-  }).catch(function (err) {
-      console.error(err);
+function start(clientN){
+  clientN.connect(1234,process.argv[3] , function() {
+    console.log('Connected');
+    
+    writePixel(picture, clientN);
   });
 };
 
@@ -64,8 +32,42 @@ function shuffle(a) {
     return a;
 }
 
-var client = new net.Socket();
 
+function parsePicture(cb){
+  Jimp.read(process.argv[2]).then(function (image) {
+    image.scan(0, 0, image.bitmap.width, image.bitmap.height, function (x, y, idx) {
+      // x, y is the position of this pixel on the image
+      // idx is the position start position of this rgba tuple in the bitmap Buffer
+      // this is the image
+
+      var red   = this.bitmap.data[ idx + 0 ];
+      var green = this.bitmap.data[ idx + 1 ];
+      var blue  = this.bitmap.data[ idx + 2 ];
+      var alpha = this.bitmap.data[ idx + 3 ];
+
+      var xoffset = x + parseInt(process.argv[4], 10);
+      var yoffset = y + parseInt(process.argv[5], 10);
+
+      //if(red != 255 && green != 255 && blue != 255){
+        data.push('PX ' + xoffset + ' ' + yoffset + ' ' + red.toString(16) + green.toString(16) + blue.toString(16)+ alpha.toString(16));
+      //}
+      
+      if(x == image.bitmap.width-1 &&
+          y == image.bitmap.height-1) {
+          console.log("Parsing picture");
+          //store result
+          picture = shuffle(data).join('\n') + '\n';
+          //writeToFile(picture);
+          cb();
+      }
+    });
+  }).catch(function (err) {
+      console.error(err);
+  });
+}
+
+//var client = new net.Socket();
+/*
 function writeToFile(data){
   console.log('Write to file');
   fs.writeFile('image.px', data, (err) => {
@@ -73,32 +75,45 @@ function writeToFile(data){
     console.log('The file has been saved!');
   });
 };
+*/
 
-function writePixel(picture){
-  client.write(picture);
+function writePixel(picture, clientN){
+  //console.log("Flut pixel with client: " + clientN);
+  //console.log(picture);
+  clientN.write(picture);
 };
 
-client.on('drain', function() {
-  writePixel(picture);
-});
-
-client.on('error', function(ex) {
-  console.log(ex);
-});
-
-client.on('data', function(data) {
-	console.log('Received: ' + data);
-	client.destroy(); // kill client after server's response
-});
-
-client.on('close', function() {
-	console.log('Connection closed');
-  start();
-});
-
 var i=0;
-while(i<parseInt(process.argv[6], 10)){
-  start();
-  i++;
-}
+parsePicture(init);
 
+function init(){
+  while(i < parseInt(process.argv[6], 10)){
+    var client = new net.Socket();
+
+    client.on('drain', function() {
+      //console.log("Flut pixel finished");
+
+      writePixel(picture, this);
+    });
+
+    client.on('error', function(ex) {
+      console.log(ex);
+    });
+
+    client.on('data', function(data) {
+      console.log('Received: ' + data);
+      client.destroy(); // kill client after server's response
+    });
+
+    client.on('close', function() {
+      console.log('Connection closed');
+      start(this);
+    });
+
+    clients.push(client);
+
+    start(client);
+    i++;
+  }
+};
+  
